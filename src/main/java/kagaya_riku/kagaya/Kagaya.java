53777eae04485a5c2b1d653634622f5b61e898e8
DESKTOP-NEE0UQS\my_ne <akym.tohoku@gmail.com>
2022-08-03 18:20:39 +0900

    実行はできた、minecraft内でエラー通知(resourses.mainがない？)

--- src/main/java/kagaya_riku/kagaya/Kagaya.java
+++ src/main/java/kagaya_riku/kagaya/Kagaya.java
@@ -1,12 +1,11 @@
 package kagaya_riku.kagaya;
 
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class Kagaya {
@@ -20,7 +19,7 @@ public class Kagaya {
     public static class Items {
         private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
         public static final RegistryObject<Item> TITANIUM_INGOT = ITEMS.register("DefaultItemName", () -> new Item(new Item.Properties()
-                .tab(ItemGroup.TAB_MATERIALS)));
+                .tab(CreativeModeTab.TAB_MATERIALS)));
 
         public static void register(IEventBus eventBus) {
             ITEMS.register(eventBus);
